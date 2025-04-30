8fff96bfe96286175aba23037115e73476c82e9f
Rohanov <arseniychekrigin@gmail.com>
2025-04-30 20:48:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const spellsData = [
     { id: 1, name: "Огненный шар", description: "Описание огненного шара", image: "img/spells/Fireball.png" },
-    { id: 2, name: "Ледяная стрела", description: "Описание ледяной стрелы", image: "img/spells/ice-arrow.png" },
+    { id: 2, name: "Ледяная стрела", description: "Описание ледяной стрелы", image: "img/spells/Arrow_of_Ice_Faded.png" },
     // ... добавьте остальные заклинания
 ];
 
