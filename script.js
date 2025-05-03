485a8fc8921763a0a3231a3b9870dc2b3b57138b
Rohanov <arseniychekrigin@gmail.com>
2025-05-03 16:00:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
 const spellsData = [
-    { id: 1, name: "Огненный шар", description: "Яркий луч вылетает из вашего указательного пальца в точку, выбранную вами в пределах дистанции, где и происходит взрыв пламени с гулким ревом. Все существа в пределах сферы с радиусом 20 футов с центром в этой точке должны совершить спасбросок Ловкости. Цель получает 8к6 урона огнём при провале или половину этого урона при успехе. Этот огонь огибает углы. Он воспламеняет горючие предметы, которые никто не несет и не носит.", image: "img/spells/Fireball.png", level: "3", school: "Воплощение", components: "В, С, М (шарик из сухой серы)", higherLevels: "Если вы накладываете это заклинание, используя ячейку 4-го уровня или выше, урон увеличивается на 1к6 за каждый уровень ячейки выше третьего." },
-    { id: 2, name: "Ледяная стрела", description: "Описание ледяной стрелы", image: "img/spells/Arrow_of_Ice_Faded.png", level: "1", school: "Воплощение", components: "В, С" },
-    { id: 3, name: "Кислотная стрела", description: "Описание кислотной стрелы", image: "img/spells/Arrow_of_Ice_Faded.png", level: "2", school: "Преобразование", components: "В, С, М (порошок, обернутый в лист)" },
-    { id: 4, name: "Щит", description: "Описание щита", image: "img/spells/Arrow_of_Ice_Faded.png", level: "1", school: "Ограждение", components: "В, С" },
-    { id: 5, name: "Луч холода", description: "Описание луча холода", image: "img/spells/Arrow_of_Ice_Faded.png", level: "0", school: "Воплощение", components: "В, С" },
-    { id: 6, name: "Магическая рука", description: "Описание магической руки", image: "img/spells/Arrow_of_Ice_Faded.png", level: "0", school: "Преобразование", components: "В, С" },
-    { id: 7, name: "Невидимость", description: "Описание невидимости", image: "img/spells/Arrow_of_Ice_Faded.png", level: "2", school: "Иллюзия", components: "В, С, М (ресница, покрытая смолой)", concentration: true, ritual: false },
-    { id: 8, name: "Иллюзорный двойник", description: "Описание иллюзорного двойника", image: "img/spells/Arrow_of_Ice_Faded.png", level: "3", school: "Иллюзия", components: "В, С, М (немного шерсти)" },
-    { id: 9, name: "Гроза", description: "Описание грозы", image: "img/spells/Arrow_of_Ice_Faded.png", level: "4", school: "Воплощение", components: "В, С, М (капля воды)" },
-    { id: 10, name: "Стена огня", description: "Описание стены огня", image: "img/spells/Arrow_of_Ice_Faded.png", level: "4", school: "Воплощение", components: "В, С, М (красный камень)" },
-    { id: 11, name: "Изгнание", description: "Описание изгнания", image: "img/spells/Arrow_of_Ice_Faded.png", level: "5", school: "Ограждение", components: "В, С, М (священный символ)" },
-    { id: 12, name: "Замедление", description: "Описание замедления", image: "img/spells/Arrow_of_Ice_Faded.png", level: "3", school: "Преобразование", components: "В, С, М (кристалл, песок, капля воды)" },
-    { id: 13, name: "Превращение", description: "Описание превращения", image: "img/spells/Arrow_of_Ice_Faded.png", level: "4", school: "Преобразование", components: "В, С, М (гусеница)" },
-    { id: 14, name: "Остановка времени", description: "Описание остановки времени", image: "img/spells/Arrow_of_Ice_Faded.png", level: "9", school: "Преобразование", components: "В" }
+    const spellsData = [
+    //{ id: , name: "", description: "", image: "img/spells/.png", level: "", school: "", components: "", higherLevels: "" castingTime: "", range: "", duration: "", classes: "", concentration: , ritual: },
+{ id: 1, name: "Брызги кислоты", description: "Вы кидаете кислотный шарик. Выберите одно существо, которое вы видите в пределах дистанции, или два существа, которых вы видите в пределах дистанции, находящихся в пределах 5 футов друг от друга. Цель должна преуспеть в спасброске Ловкости, иначе получит 1к6 урона кислотой.
+Урон этого заклинания увеличивается на 1к6, когда вы достигаете 5-го уровня (2к6), 11-го уровня (3к6) и 17-го уровня (4к6).", image: "img/spells/Acid_Splash.png", level: "0", school: "Вызов", components: "В, С", castingTime: "1 действие", range: "60 футов", duration: "Мгновенная", classes: "волшебник, изобретатель, чародей", concentration: false, ritual: false}
 ];
 
 const bestiaryData = [
